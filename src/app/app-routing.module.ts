86d2ed58c3f7ca270b43790f1795148dba811675
Mostafa Tarek <mostafa.tarek.abdeljawad@gmail.com>
2026-06-22 01:01:50 +0300

    added cart list items view and service logic of the checkout and clear

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './product/product-list/product-list.component';
+import { CartViewComponent } from './cart/cart-view/cart-view.component';
 
 const routes: Routes = [
   {path:'', redirectTo: '/products', pathMatch:'full'}, //if empty go to /products
-  {path:'products', component: ProductListComponent}
+  {path:'products', component: ProductListComponent}, 
+  {path:'cart', component:CartViewComponent}
 
 ];
 
